416a11448ac63df1fb3a01c77a2133758b88e070
Kaz. HIDA <kazhida@abplus.com>
2013-05-08 15:39:20 +0900

    Preferenceまわりを作り直した。

--- src/com/abplus/dashbacklog/MainActivity.java
+++ src/com/abplus/dashbacklog/MainActivity.java
@@ -3,17 +3,29 @@ package com.abplus.dashbacklog;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.View;
+import android.view.*;
 
 public class MainActivity extends Activity {
 
+    private enum ListOf {
+        SUMMARIES,
+        TIME_LINE,
+        COMMENTS
+    }
+
     private BacklogIO backlog = null;
     private BackLogCache cache = null;
+    private ListOf which = ListOf.SUMMARIES;
+    private MenuItem postItem = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            setTheme(R.style.app_theme);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
@@ -39,15 +51,13 @@ public class MainActivity extends Activity {
         String space_id = prefs.getString(getString(R.string.key_space_id), "");
         String user_id = prefs.getString(getString(R.string.key_user_id), "");
         String password = prefs.getString(getString(R.string.key_password), "");
-        int which = prefs.getInt(getString(R.string.key_displayed), -1);
+        int displayed = prefs.getInt(getString(R.string.key_displayed), -1);
 
         if (space_id.length() == 0 || user_id.length() == 0 || password.length() == 0) {
-            Intent intent=new Intent(this, PrefsActivity.class);
-            intent.setAction(Intent.ACTION_VIEW);
-            startActivity(intent);
+            showPreferences();
         } else {
             backlog = new BacklogIO(space_id, user_id, password);
-            if (which == ListOf.SUMMARIES.ordinal()) {
+            if (displayed != ListOf.TIME_LINE.ordinal()) {
                 showSummaries();
             } else {
                 showTimeLine();
@@ -62,36 +72,71 @@ public class MainActivity extends Activity {
         //  どれを表示していたかを保存しておく
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putInt(getString(R.string.key_displayed), whichDisplayed().ordinal());
+        editor.putInt(getString(R.string.key_displayed), which.ordinal());
+        editor.commit();
+    }
+
+    private void showPreferences() {
+        Intent intent=new Intent(this, PrefsActivity.class);
+        intent.setAction(Intent.ACTION_VIEW);
+        startActivity(intent);
     }
 
     private void showSummaries() {
+        which = ListOf.SUMMARIES;
+
         findViewById(R.id.tab_summaries).setBackgroundColor(getResources().getColor(R.color.bg_selected));
         findViewById(R.id.tab_time_line).setBackgroundColor(getResources().getColor(R.color.bg_unselected));
+        postItem.setVisible(false);
     }
 
     private void showTimeLine() {
+        which = ListOf.TIME_LINE;
+
         findViewById(R.id.tab_summaries).setBackgroundColor(getResources().getColor(R.color.bg_unselected));
         findViewById(R.id.tab_time_line).setBackgroundColor(getResources().getColor(R.color.bg_selected));
+        postItem.setVisible(false);
     }
 
     private void showComments(int commentId) {
+        which = ListOf.COMMENTS;
 
+        postItem.setVisible(true);
     }
 
-    private enum ListOf {
-        SUMMARIES,
-        TIME_LINE,
-        COMMENTS
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+
+        postItem = menu.findItem(R.id.menu_post);
+
+        return true;
     }
 
-    private ListOf whichDisplayed() {
-        if (findViewById(R.id.tabs).getVisibility() == View.GONE) {
-            return ListOf.COMMENTS;
-        } else if (findViewById(R.id.tab_summaries).getTag() != null) {
-            return ListOf.SUMMARIES;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_config:
+                showPreferences();
+                return true;
+            case R.id.menu_reload:
+                return true;
+            case R.id.menu_post:
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && which == ListOf.COMMENTS) {
+            showTimeLine();
+            return true;
         } else {
-            return ListOf.TIME_LINE;
+            return super.onKeyDown(keyCode, event);
         }
     }
 }
--- src/com/abplus/dashbacklog/PrefsActivity.java
+++ src/com/abplus/dashbacklog/PrefsActivity.java
@@ -1,10 +1,12 @@
 package com.abplus.dashbacklog;
 
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-
-import java.util.List;
+import android.app.Activity;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * Copyright (C) 2013 ABplus Inc. kazhida
@@ -12,25 +14,55 @@ import java.util.List;
  * Author:  kazhida
  * Created: 2013/05/08 12:09
  */
-public class PrefsActivity extends PreferenceActivity {
+public class PrefsActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-            oldStylePreferencesFromResource();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            setTheme(R.style.app_theme);
         }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prefs);
+
+        findViewById(R.id.save_prefs).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences prefs = getPreferences();
+                SharedPreferences.Editor editor = prefs.edit();
+
+                editor.putString(getString(R.string.key_space_id), getEditText(R.id.space_id));
+                editor.putString(getString(R.string.key_user_id),  getEditText(R.id.user_id));
+                editor.putString(getString(R.string.key_password), getEditText(R.id.password));
+
+                editor.commit();
+
+                //  閉じる
+                finish();
+            }
+        });
     }
 
     @Override
-    public void onBuildHeaders(List<Header> target) {
-        super.onBuildHeaders(target);
-        loadHeadersFromResource(R.xml.prefs_fragment, target);
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        setEditText(R.id.space_id, prefs.getString(getString(R.string.key_space_id), ""));
+        setEditText(R.id.user_id, prefs.getString(getString(R.string.key_user_id), ""));
+        setEditText(R.id.password, prefs.getString(getString(R.string.key_password), ""));
     }
 
-    @SuppressWarnings("deprecation")
-    private void oldStylePreferencesFromResource() {
-        addPreferencesFromResource(R.xml.prefs);
+    private void setEditText(int id, String text) {
+        EditText edit = (EditText)findViewById(id);
+        edit.setText(text);
+    }
+
+    private String getEditText(int id) {
+        EditText edit = (EditText)findViewById(id);
+        return edit.getText().toString();
+    }
+
+    private SharedPreferences getPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(this);
     }
 }
